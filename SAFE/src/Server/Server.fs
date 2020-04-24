25b1263c23df285f85bb05c7ced4fb2c2b763f05
Jonas Juselius <jonas.juselius@itpartner.no>
2020-04-24 18:56:28 +0200

    Add Authentication

--- SAFE/src/Server/Server.fs
+++ SAFE/src/Server/Server.fs
@@ -2,12 +2,24 @@ open System
 open System.IO
 
 open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Authentication
+open Microsoft.AspNetCore.Authentication.Cookies
 open Microsoft.Extensions.DependencyInjection
+open System.Security.Claims
+open FSharp.Control.Tasks.V2
 
 open Giraffe
 open Api
+
+// in production, use bcrypt hasing for passwords!
+let users = [
+    "a", "b"
+    "admin@acme.com", "secret"
+    "user@contoso.com", "123"
+]
 
 let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
 
@@ -21,28 +33,82 @@ let port =
     "SERVER_PORT"
     |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 8085us
 
+let cookieChallenge (next : HttpFunc) (ctx : HttpContext) =
+    printfn "cookie challenge"
+    challenge CookieAuthenticationDefaults.AuthenticationScheme next ctx
+
+let authorize (next : HttpFunc) (ctx : HttpContext) =
+    requiresAuthentication cookieChallenge next ctx
+
+let signIn (next : HttpFunc) (ctx : HttpContext) =
+    task {
+        let! user, password = ctx.BindJsonAsync<string * string> ()
+        if List.exists ((=) (user, password)) users then // not for production!
+            let claims = [ Claim (ClaimTypes.Name, user) ]
+            let identity = ClaimsIdentity (claims, CookieAuthenticationDefaults.AuthenticationScheme)
+            let principal = ClaimsPrincipal identity
+            do! ctx.SignInAsync (CookieAuthenticationDefaults.AuthenticationScheme, principal)
+            return! json (Some user) next ctx
+        else
+            return! json None next ctx
+    }
+
+let signOut (next : HttpFunc) (ctx : HttpContext) =
+        task {
+            do! ctx.SignOutAsync()
+            return! next ctx
+        }
+
+let getUser (next : HttpFunc) (ctx : HttpContext) =
+    let user =
+        if ctx.User.Identity.IsAuthenticated then
+            Some ctx.User.Identity.Name
+        else
+            None
+    task {
+        return! json user next ctx
+    }
+
 let webApp : HttpHandler =
     choose [
-        GET >=> route "/api/init" >=> handleInit
-        GET >=> route "/api/people" >=> handleGetPeople
-        GET >=> routef "/api/person/%s/%s" handleGetPerson
-        route "/api/person"
-            >=> choose [
-                POST   >=> handleAddPerson
-                PUT    >=> handleUpdatePerson
-                DELETE >=> handleDeletePerson
-            ]
+        choose [
+            route "/api/me" >=> getUser
+            route "/api/logout" >=> signOut >=> redirectTo false "/"
+            route "/api/init" >=> handleInit
+            route "/api/people" >=> handleGetPeople
+            routef "/api/person/%s/%s" handleGetPerson
+            POST >=> route "/api/login" >=> signIn
+        ]
+        authorize >=> choose [
+            route "/api/person"
+                >=> choose [
+                    POST   >=> handleAddPerson
+                    PUT    >=> handleUpdatePerson
+                    DELETE >=> handleDeletePerson
+                ]
+        ]
     ]
 
 let jsonSerializer = Thoth.Json.Giraffe.ThothSerializer ()
 
+let authenticationOptions (opt : AuthenticationOptions) =
+    opt.DefaultScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+
+let cookieOptions = Action<CookieAuthenticationOptions> (fun options ->
+    options.LoginPath <- PathString "/signin")
+
 let configureServices (services : IServiceCollection) =
     services.AddGiraffe() |> ignore
     services.AddSingleton<Serialization.Json.IJsonSerializer>(jsonSerializer) |> ignore
+    services.AddAuthentication(authenticationOptions)
+        .AddCookie(cookieOptions)
+        |> ignore
 
 let configureApp (app : IApplicationBuilder) =
     app.UseDefaultFiles()
        .UseStaticFiles()
+       .UseCookiePolicy()
+       .UseAuthentication()
        .UseGiraffe webApp
 
 Db.tryMigrate ()
