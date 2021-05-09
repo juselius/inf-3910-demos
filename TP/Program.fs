d7559e6691470603e6daaed9cbaee04c48c654d0
Jonas Juselius <jonas.juselius@itpartner.no>
2021-05-09 16:30:12 +0200

    Add type provicer demo

---
+++ TP/Program.fs
@@ -0,0 +1,13 @@
+﻿module TP
+
+open System
+open FSharp.Data
+
+type Stocks =  CsvProvider<"stocks.csv">
+
+[<EntryPoint>]
+let main argv =
+
+    let stocks = Stocks.Load("stocks.csv")
+    Seq.iter (printfn "%A") stocks.Rows 
+    0 // return an integer exit code
---
+++ TP/TP.fs
@@ -0,0 +1,13 @@
+﻿module TP
+
+open System
+open FSharp.Data
+
+type Stock = CsvProvider<"stocks.csv">
+
+[<EntryPoint>]
+let main argv =
+    let x = Stock.Load("stocks.csv")
+    let r = Seq.head x.Rows
+    printfn "%A" r.High
+    0 // return an integer exit code
